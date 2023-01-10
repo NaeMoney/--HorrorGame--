19340f049551629fe11662343dac8267d6aa1759
NaeMoney <c.larkin69@yahoo.com>
2023-01-10 19:20:03 +0000

    Updated the AI and added more SFX

---
+++ HorrorCollab/Assets/Horror FPS KIT/HFPS Assets/Content/Scripts/Core/Examples/EmeraldAISendDamage.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using EmeraldAI;
+using HFPS.Player;
+
+namespace ThunderWire.EmeraldAI
+{
+    public class EmeraldAISendDamage : MonoBehaviour
+    {
+        /// <summary>
+        /// Send Damage to Emerald AI System
+        /// </summary>
+        public void ApplyDamage(int DamageAmount)
+        {
+            EmeraldAISystem EmeraldComponent = GetComponent<EmeraldAISystem>();
+            PlayerController player = PlayerController.Instance;
+
+            //Cause damage to the AI that is hit using the HFPS damage function.
+            EmeraldComponent.Damage(DamageAmount, EmeraldAISystem.TargetType.Player, player.transform, 400);
+        }
+    }
+}
